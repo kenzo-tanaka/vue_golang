beba3185f61e23eb93cbb5e90d5204b81a8b14ff
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-08-06 20:54:15 +0900

    add: signin and signup pages

---
+++ src/components/Signin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="signin">
+    <h2>Sign in</h2>
+    <input type="text" placeholder="email" v-model="email">
+    <input type="password" placeholder="password" v-model="password">
+    <button>SignIn</button>
+    <p>You don't have an account?</p>
+    <router-link to="/signup">Create account now!</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Signin",
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.signin {
+  margin-top: 20px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
