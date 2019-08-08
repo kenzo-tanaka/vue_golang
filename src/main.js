ede85bf8993d50a626930a3c29c8d99f3c03c722
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-08-08 09:14:14 +0900

    fix: main.js (firebae configを追記)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyAEILra_CTqmKMXK6yIVG0bPvHhx72B8b4",
+  authDomain: "vue-and-golang-9dbdd.firebaseapp.com",
+  databaseURL: "https://vue-and-golang-9dbdd.firebaseio.com",
+  projectId: "vue-and-golang-9dbdd",
+  storageBucket: "",
+  messagingSenderId: "946274130404"
+};
+
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
